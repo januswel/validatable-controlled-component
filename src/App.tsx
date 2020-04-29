6d041a6438576a8c778ff4ac3a2e4808bba76b40
Takagi Kensuke <janus.wel.3@gmail.com>
2020-04-29 16:14:36 +0900

    :sparkles: define custom hook useControlledComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "96%",
+    borderWidth: 1,
+    fontSize: 24,
+  },
+});
+
+function useControlledComponent() {
+  const [value, setValue] = React.useState("");
+  function onChangeText(newValue: string) {
+    setValue(newValue);
+  }
+  return {
+    value,
+    onChangeText,
+  };
+}
 
 export default function App() {
+  const name = useControlledComponent();
+
   return (
-    <View>
-      <Text>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </Text>
+    <View style={styles.container}>
+      <TextInput {...name} style={styles.input} />
     </View>
   );
 }
