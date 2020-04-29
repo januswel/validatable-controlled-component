fd02bb656b67c3bbd13c56a969d8dd6e12b3438e
Takagi Kensuke <janus.wel.3@gmail.com>
2020-04-29 15:49:44 +0900

    :tada: setup minimum

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Text, View } from "react-native";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <View>
+      <Text>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </Text>
+    </View>
   );
 }
-
-export default App;
